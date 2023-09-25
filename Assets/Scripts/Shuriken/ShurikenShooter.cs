884d36002e2901cecd553ad07fbd47ec1e9396f8
bagzaru <bagzaru3690@gmail.com>
2023-09-25 15:09:26 +0900

    [Feat]마우스 클릭 위치로 발사, 발사 시 레이어 설정 가능하게 변경

--- Assets/Scripts/Shuriken/ShurikenShooter.cs
+++ Assets/Scripts/Shuriken/ShurikenShooter.cs
@@ -7,15 +7,29 @@ using CoroutineRunner = Unity.VisualScripting.CoroutineRunner;
 public class ShurikenShooter : MonoBehaviour
 {
 	public TestMover shurikenPrefab;
+	public LayerMask damageLayer;
 
 	[Header("플레이어에게서 약간 떨어진 거리에서 발사됩니다.")]
 	public float shootRadius = 0.5f;
+
+	#region privateArea
+
+	private Camera camera;
 	
+
+	#endregion
+	
+	private void Start()
+	{
+		camera = Camera.main;
+	}
+
 	private void Update()
 	{
+		//테스트용, 나중에 Input System으로 변경 필요
 		if (Input.GetMouseButtonDown(0))
 		{
-			Shoot(Vector2.right);
+			Shoot(camera.ScreenToWorldPoint(Input.mousePosition) - transform.position);
 		}
 	}
 
@@ -25,5 +39,7 @@ public class ShurikenShooter : MonoBehaviour
 		TestMover inst = Instantiate(shurikenPrefab, (Vector2)transform.position + _dir*shootRadius, Quaternion.identity);
 		inst.direction = _dir;
 		inst.SetRotationByDirection();
+		Shuriken instSrk = inst.GetComponent<Shuriken>();
+		instSrk.damageLayer = damageLayer;
 	}
 }
