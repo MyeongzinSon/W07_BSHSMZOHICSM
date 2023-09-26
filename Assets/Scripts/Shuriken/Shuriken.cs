f5d32ed6d59fef3b4f33d670750ad6ea400fb2fa
haein Oh <badarangdev@gmail.com>
2023-09-26 20:45:28 +0900

    321때 못쏘게 하기 수정. 드롭 아이템 수정

--- Assets/Scripts/Shuriken/Shuriken.cs
+++ Assets/Scripts/Shuriken/Shuriken.cs
@@ -158,7 +158,7 @@ public class Shuriken : MonoBehaviour
             AdaptBoomerangReturnMove();
         }
 
-        if (state == ShurikenState.PICKUP)
+        if (state == ShurikenState.PICKUP && owner.tag == "Player")
         {
             spriteOutline.UpdateOutline(true);
             return;
