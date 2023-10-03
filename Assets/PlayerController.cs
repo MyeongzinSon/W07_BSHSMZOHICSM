e44be151a9a5c6f1b3f71b0384558e715957504d
Cho_SungMin <58628847+ahsjdkflgh3041@users.noreply.github.com>
2023-10-03 12:01:38 +0900

    [Fix] Cam

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -37,7 +37,8 @@ public class PlayerController : MonoBehaviour, NewInputActions.IPlayerActions
 			cameraExpandTimer += Time.deltaTime;
 			if (cameraExpandTimer >= cameraExpandDelay)
 			{
-				VCamManager.Instance.Expand();
+				// set PlayerNum
+				VCamManager.Instance.Expand(0);
 			}
 		}
 	}
