99c5778cc8b5e4407769f7370b94f92a76786ed1
bagzaru <bagzaru3690@gmail.com>
2023-10-05 09:33:12 +0900

    [Feat]: 슬로우 이동속도도 느려짐

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,12 @@ public class PlayerMove : Mover
 {
     [Header("넉백 관련")]
     public float knockbackTime = 0.25f;
+    [Header("슬로우 관련")]
+    public float slowRatio = 0.2f;
+    private int slowCount = 0;
     //public Vector2 lastInput;
+    
+    
 
     public void OnMove(InputAction.CallbackContext _callback)
     {
@@ -17,6 +23,28 @@ public class PlayerMove : Mover
         {
             lastInput = direction;
         }
+    }
+
+    public override void Move()
+    {
+        if (body == null)
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+        
+        //슬로우라면 슬로우 비율 지정해서 슬로우
+        if(slowCount>0)
+            body.MovePosition(body.position + (1f-slowRatio)*speed * Time.fixedDeltaTime * direction.normalized);
+        else
+            body.MovePosition(body.position + speed * Time.fixedDeltaTime * direction.normalized);
+        
+        
+        if (isRotate)
+        {
+            //Direction방향으로 Rotation값 수정
+            SetRotationByDirection();
+        }
+        //body.velocity = direction.normalized * speed;
     }
 
 
@@ -36,5 +64,23 @@ public class PlayerMove : Mover
         CanMove = false;
         yield return new WaitForSeconds(knockbackTime);
         CanMove = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("SpiderWeb"))
+        {
+            //스파이더웹이라면 슬로우 On
+            slowCount += 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("SpiderWeb"))
+        {
+            //스파이더웹이라면 슬로우 On
+            slowCount -= 1;
+        }
     }
 }
